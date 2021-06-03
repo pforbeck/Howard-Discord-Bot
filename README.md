2485b1aab3ef95071a3cd0a5ea7426a7d67ec974
Pat Forbeck <pat@forbeck.com>
2021-06-03 00:21:12 -0500

    commit on 'main'

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Howard-Discord-Bot
-Discord Bot experiment written in JS by Pat Forbeck
+Discord Bot experiment written in JS by Pat Forbeck.
+This was made as a learning experience for both JS, the Discord API, and Github.
+This is my first JS or Github project ever and should be taken as such.
--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const fs = require('fs')
 const Discord = require("discord.js");
 const config = require("./config.json");
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 client.login(config.BOT_TOKEN);
+
+client.once('ready', () => {
+    console.log('Ready!');
+  });
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for(const file of commandFiles){
+  const command = require(`./commands/${file}`);
+   client.commands.set(command.name, command);
+}
+
+  // https://stackoverflow.com/questions/65509847/discord-bot-crashes-when-a-command-executed-in-discord-js
