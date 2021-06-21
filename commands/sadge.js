20d704595f5292ff343aa174cc7b31806369d441
Pat Forbeck <pat@forbeck.com>
2021-06-21 18:49:30 -0500

    changed 'forsan' to 'forsen'

--- commands/sadge.js
+++ commands/sadge.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'sadge',
 	execute(msg) {
 		var int = (Math.random() * (5 - 0 + 1) ) << 0
-		var responses = ["verily ", "omegalul ", "nomegalul ", "supertf died attempting to outhrow forsan... ", "fuck xqc ", "whatever you say pal "];
+		var responses = ["verily ", "omegalul ", "nomegalul ", "supertf died attempting to outhrow forsen... ", "fuck xqc ", "whatever you say pal "];
 		msg.reply(responses[int], {files: ["./command_additional_files/howards/howard_stressed.png"]});
 	},
 };
