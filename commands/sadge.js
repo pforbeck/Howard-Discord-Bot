5b27423ba94fdf4850578b2a6c42b8127b47a143
Pat Forbeck <pat@forbeck.com>
2021-06-23 00:12:20 -0500

    updated 'sadge' command

--- commands/sadge.js
+++ commands/sadge.js
@@ -2,8 +2,8 @@ module.exports = {
 	name: 'sadge',
 	description: 'sadge',
 	execute(msg) {
-		var int = (Math.random() * (5 - 0 + 1) ) << 0
 		var responses = ["verily ", "omegalul ", "nomegalul ", "supertf died attempting to outhrow forsen... ", "fuck xqc ", "whatever you say pal "];
-		msg.reply(responses[int], {files: ["./command_additional_files/howards/howard_stressed.png"]});
+		var int = (Math.random() * (responses.length) ) << 0
+		msg.channel.send(responses[int], {files: ["./command_additional_files/howards/howard_stressed.png"]});
 	},
 };
