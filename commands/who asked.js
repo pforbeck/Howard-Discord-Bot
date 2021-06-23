a5f2b7d33361dc9949ee73e5a59eb947c8fa8684
Pat Forbeck <pat@forbeck.com>
2021-06-23 01:05:12 -0500

    updated 'who asked' command

--- commands/who asked.js
+++ commands/who asked.js
@@ -2,8 +2,16 @@ module.exports = {
 	name: 'who asked',
 	description: 'who asked',
 	execute(msg) {
-		var responses = ["yikers ", "schut ", "sus ", "bonk go to horny jail "];
-		var int = (Math.random() * (responses.length) ) << 0
-		msg.channel.send(responses[int], {files: ["./command_additional_files/howards/howard_with_gun.png"]});
+		var responses = ["yikers ", "schut ", "sus "];
+		var who_asked = [
+		"./command_additional_files/who asked/howard_with_gun.png", 
+		"./command_additional_files/who asked/40f.png", 
+		"./command_additional_files/who asked/692.png", 
+		"./command_additional_files/who asked/HowardFear.png"];
+
+		var int1 = (Math.random() * (responses.length) ) << 0
+		var int2 = (Math.random() * (who_asked.length) ) << 0
+		
+		msg.channel.send(responses[int1], {files: [who_asked[int2]]});
 	},
 };
