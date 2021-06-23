4b8d892c80f44199a5b7ef3264838b8418cec9b7
Pat Forbeck <pat@forbeck.com>
2021-06-23 00:09:44 -0500

    updated 'dragon' command

--- commands/dragon.js
+++ commands/dragon.js
@@ -2,8 +2,8 @@ module.exports = {
 	name: 'dragon',
 	description: 'dragon',
 	execute(msg) {
-		var int = (Math.random() * (responses.length - 0 + 1) ) << 0
 		var responses = ["in your mouth ", "on your face ", "down your throat ", "in your hands", "on your forehead ", "etc... "];
-		msg.reply("dragon deez nutz " + responses[int]);
+		var int = (Math.random() * (responses.length) ) << 0
+		msg.channel.send("dragon deez nutz " + responses[int]);
 	},
 };
