6ca34f15c239331c837ba732b2ef8c172ae86aaf
Pat Forbeck <pat@forbeck.com>
2021-06-23 00:12:11 -0500

    updated 'parody' command

--- commands/parody.js
+++ commands/parody.js
@@ -2,8 +2,8 @@ module.exports = {
 	name: 'parody',
 	description: 'parody',
 	execute(msg) {
-		var int = (Math.random() * (responses.length - 0 + 1) ) << 0
 		var responses = ["in your mouth ", "on your face ", "down your throat ", "in your hands", "on your forehead ", "etc... "];
-		msg.reply("parody nutz " + responses[int]);
+		var int = (Math.random() * (responses.length) ) << 0
+		msg.channel.send("parodeez nutz " + responses[int]);
 	},
 };
