6af9e06688060c05373803def3222482c0a1d412
Pat Forbeck <pat@forbeck.com>
2021-06-23 00:10:46 -0500

    removed 'dragons' (conflict w/ 'imagine dragons')

--- commands/dragons.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'dragons',
-	description: 'dragons',
-	execute(msg) {
-		var int = (Math.random() * (responses.length - 0 + 1) ) << 0
-		var responses = ["in your mouth ", "on your face ", "down your throat ", "in your hands", "on your forehead ", "etc... "];
-		msg.reply("dragon deez nutz " + responses[int]);
-	},
-};
