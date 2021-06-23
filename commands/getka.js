b106e4522110eae94eadb07d30b83e166c8f72ea
Pat Forbeck <pat@forbeck.com>
2021-06-23 00:11:13 -0500

    updated 'getka' command

--- commands/getka.js
+++ commands/getka.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'getka',
 	execute(msg) {
 		var responses = ["hmmmm ", "tasty ", "yikers ", "my highschool CS teacher ladies and gentlemen ", "sus ", "bonk go to horny jail "];
-		var int = (Math.random() * (responses.length - 0 + 1) ) << 0
-		msg.reply(responses[int], {files: ["./command_additional_files/getka/getka.mp4"]});
+		var int = (Math.random() * (responses.length) ) << 0
+		msg.channel.send(responses[int], {files: ["./command_additional_files/getka/getka.mp4"]});
 	},
 };
