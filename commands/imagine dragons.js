e7251e5b443b223f88aa4403984cb8008f8e813a
Pat Forbeck <pat@forbeck.com>
2021-06-23 00:11:46 -0500

    updated 'imagine dragons' command

--- commands/imagine dragons.js
+++ commands/imagine dragons.js
@@ -1,9 +1,9 @@
 module.exports = {
-	name: 'parody',
-	description: 'parody',
+	name: 'imagine dragons',
+	description: 'imagine dragons',
 	execute(msg) {
-		var int = (Math.random() * (responses.length - 0 + 1) ) << 0
 		var responses = ["in your mouth ", "on your face ", "down your throat ", "in your hands", "on your forehead ", "etc... "];
-		msg.reply("parody nutz " + responses[int]);
+		var int = (Math.random() * (responses.length) ) << 0
+		msg.channel.send("imagine dragon deez nutz " + responses[int]);
 	},
 };
