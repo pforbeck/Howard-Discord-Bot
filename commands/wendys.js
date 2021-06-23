120fff609c0a1a011a3180236dddfb12845e66d2
Pat Forbeck <pat@forbeck.com>
2021-06-23 00:12:42 -0500

    updated 'wendys' command

--- commands/wendys.js
+++ commands/wendys.js
@@ -2,8 +2,8 @@ module.exports = {
 	name: 'wendys',
 	description: 'wendys',
 	execute(msg) {
-		var int = (Math.random() * (5 - 0 + 1) ) << 0
 		var responses = ["in your mouth ", "on your face ", "down your throat ", "in your hands", "on your forehead ", "etc... "];
-		msg.reply("wendys nutz go " + responses[int]);
+		var int = (Math.random() * (responses.length) ) << 0
+		msg.channel.send("wendeez nutz go " + responses[int]);
 	},
 };
