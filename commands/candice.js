660e53eddca87abb7e7130fddd6dfbf15e2f9f7a
Pat Forbeck <pat@forbeck.com>
2021-06-23 00:09:06 -0500

    updated 'candice' command

--- commands/candice.js
+++ commands/candice.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'candice',
 	execute(msg) {
 		var responses = ["dick fit in your mouth", "dick sit on your face", "dick go down your throat", "dick go in your hands", "dick go on your forehead", "etc..."];
-		var int = (Math.random() * (responses.length - 0 + 1) ) << 0
-		msg.reply("candice " + responses[int]);
+		var int = (Math.random() * (responses.length) ) << 0
+		msg.channel.send("candice " + responses[int]);
 	},
 };
