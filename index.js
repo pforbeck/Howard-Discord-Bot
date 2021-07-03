45a1487040243b38c036afdc4b7a50911f6605df
Pat Forbeck <pat@forbeck.com>
2021-07-02 21:33:21 -0500

    cleaned up code significantly

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const fs = require('fs');
 const Discord = require("discord.js"); // A JS implementation of the Discord API
-const shell = require('shelljs'); // To execute shell commands
 const config = require("./config.json"); // Handles private variables
-const request = require('request'); // Handles requests
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-const person = client.users.cache.get(config.MY_ID);
 
 // Logs the bot in
 client.login(config.BOT_TOKEN); 
@@ -71,10 +68,6 @@ function gamerMoments(){
   }
 }
 
-function capitalizeFirstLetter(string) { // Capitalizes first letter of any given string
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
 // Bot is ready to use
 client.on('ready', () => { 
   client.user.setActivity('Ape Escape', { type: 'PLAYING' }) // Sets activity
@@ -90,10 +83,9 @@ client.on('error', (err) => {
     console.error(err);
 });
 
-// Actual bot code below
-
 // Finds files in the command folder that end with ".js"
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js')) 
+
 // Indexes the ".js" files findable so that they can be executed
 for(const file of commandFiles){
   // Checks commands folder for corresponding file
@@ -104,70 +96,27 @@ for(const file of commandFiles){
 // Code for calling commands (user says ping, we say pong)
 client.on('message', msg => {
 
-  // Starts MC Server
-  if (msg.content == "!startmc"){ 
-    msg.reply("MC Server is starting!");
-    shell.exec('bash -c /home/pat/git-projects/Howard-Discord-Bot/commands/startmc.sh')
-  }
-
-  // Stops MC Server
-  if (msg.content == "!stopmc"){
-    msg.reply("MC Server is dead...");
-    shell.exec('bash -c /home/pat/git-projects/Howard-Discord-Bot/commands/stopmc.sh')
-  }
-
-  // Checks Github Status
-  if (msg.content == "!github"){
-    request('https://www.githubstatus.com/',  { json: true }, (err, res, body) => {
-      let githubStatus = new Discord.MessageEmbed()
-	    .setColor('#0099ff')
-	    .setTitle('GitHub Status')
-	    .setURL(body.page.url)
-	    .setThumbnail('https://i.imgur.com/O3WvuHp.png')
-	    .addFields(
-        { name: 'GitHub System Status', value: body.status.description},
-        { name: '\u200B', value: '\u200B' },
-	    	{ name: body.components[0].name, value: capitalizeFirstLetter(body.components[0].status), inline: true },
-	    	{ name: body.components[1].name, value: capitalizeFirstLetter(body.components[1].status), inline: true },
-		    { name: body.components[2].name, value: capitalizeFirstLetter(body.components[2].status), inline: true },
-        { name: body.components[4].name, value: capitalizeFirstLetter(body.components[4].status), inline: true },
-		    { name: body.components[5].name, value: capitalizeFirstLetter(body.components[5].status), inline: true },
-        { name: body.components[6].name, value: capitalizeFirstLetter(body.components[6].status), inline: true },
-		    { name: body.components[7].name, value: capitalizeFirstLetter(body.components[7].status), inline: true },
-        { name: body.components[8].name, value: capitalizeFirstLetter(body.components[8].status), inline: true },
-	    )
-	    .setTimestamp()
-	    .setFooter('Brought to you by Howard', 'https://i.imgur.com/O3WvuHp.png');
-      msg.channel.send(githubStatus);
-    });
-  }
-
   // Checks that the incoming message has the specified prefix and does not originate from the bot
-  if (!msg.content.startsWith(config.PREFIX) || msg.author.bot) return;
+  if ((!msg.content.startsWith(config.PREFIX) && !msg.content.startsWith(config.PREFIX2)) || msg.author.bot) return;
 
     // Makes incoming sentence lowercase and makes the sentence an array of words
-    const message = msg.content.toLowerCase(); 
     const args = msg.content.slice(config.PREFIX.length).trim().split(/ +/); // Makes 
 
     // Iterates through the array of words
     for (var i = 0; i < args.length; i++) { 
-      // Makes the word lowercase, removes non-alphanumeric characters, and removes punctuation
+      // Makes the word lowercase, removes non-alphanumeric characters, and removes punctuation (except for '!')
       args[i] = args[i].toLowerCase(); 
-      // For debugging
+      args[i] = args[i].replace(/[.,\/#$%\^&\*;:{}=\-_`~()]/g, "");
       console.log(args[i])
-      args[i] = args[i].replace(/[^0-9a-z]/gi, ""); 
-      args[i] = args[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
       
       try {
         // Attemps to retrieve and execute a one word file associated with the command
         client.commands.get(args[i]).execute(msg, args);
-        // It worked
         console.log("- yea");
       } catch (error) {
         try {
           // Attemps to retrieve and execute a two word file associated with the command
           client.commands.get(args[i] + " " + args[i+1]).execute(msg, args);
-          // It worked
           console.log("- yea");
         } catch (error) {
           // The word doesn't have a corresponding ".js" file or an exception occured
