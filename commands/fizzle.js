3a71ad22498b399e9d797e463c19d7a3812eff96
Pat Forbeck <pat@forbeck.com>
2021-07-10 05:45:45 -0500

    optimized "fizzle" command

--- commands/fizzle.js
+++ commands/fizzle.js
@@ -7,9 +7,9 @@ module.exports = {
 		"https://preview.redd.it/cfy0e9pog9m51.jpg?width=640&crop=smart&auto=webp&s=74eb6c8f84598640e6267019f470e5c62a2e3696",
 		"https://preview.redd.it/cfy0e9pog9m51.jpg?width=640&crop=smart&auto=webp&s=74eb6c8f84598640e6267019f470e5c62a2e3696",
 		"https://preview.redd.it/ozajwcuta8d51.jpg?width=960&crop=smart&auto=webp&s=2d770e485f4854bff146d17cb76addc0d40440d2",
-		"./command_additional_files/fizzle/1.mp4",
-		"./command_additional_files/fizzle/2.mp4",
-		"./command_additional_files/fizzle/3.mp4"]
+		"https://i.imgur.com/nYowMaW",
+		"https://i.imgur.com/aM9oTFS",
+		"https://i.imgur.com/B4b0rlR"]
 
 		var int1 = (Math.random() * (responses.length) ) << 0
 		var int2 = (Math.random() * (fizzle.length) ) << 0
