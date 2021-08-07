17aa92a6875b31a8ca0296512fb613c867290647
Pat Forbeck <pat@forbeck.com>
2021-08-07 02:51:27 -0500

    updated '?commands' command

--- commands/?commands.js
+++ commands/?commands.js
@@ -9,7 +9,9 @@ module.exports = {
 		.setAuthor('Howard Bot', 'https://i.imgur.com/wdDNd67.png')
 		.setThumbnail('https://i.imgur.com/wdDNd67.png')
 		.addFields(
-		{ name: 'Commands', value: '?commands\n!github\nape escape\nboth of\ncandice\ncd\ndragon\nfizzle\ngetka\nhandful\nharambe\nhorny\nhoward\nimagine dragons\nlick my\noh really\nparody\npong\nsadge\nstigma\nsuck my\nwendys\nwho asked\nxqc'},
+		{ name: 'Commands', value: '?commands\nhoward\nape escape\nboth of\ncandice\ncd\ndragon\nxqc', inline: true},
+		{ name: '-', value: 'imagine dragons\nlick my\noh really\nparody\npong\nsadge\nstigma\nfizzle', inline: true},
+		{ name: '-', value: '\ngetka\nhandful\nharambe\nhorny\nsuck my\nwendys\nwho asked\n!github', inline: true},
 		)
 		.setTimestamp()
 		.setFooter('Brought to you by Howard', 'https://i.imgur.com/wdDNd67.png');
