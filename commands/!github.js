6df2125cd42331e6108615ff6d0252a7c14c7ad5
Pat Forbeck <pat@forbeck.com>
2021-08-07 13:35:56 -0500

    updated and added commands

--- commands/!github.js
+++ commands/!github.js
@@ -11,27 +11,12 @@ module.exports = {
 
 		// Actual Code
 		// Checks Github Status
-		request('https://www.githubstatus.com/',  { json: true }, (err, res, body) => {
 		let githubStatus = new Discord.MessageEmbed()
 		.setColor('#0099ff')
-		.setTitle('GitHub Status')
-		.setURL(body.page.url)
-		.setThumbnail('https://i.imgur.com/O3WvuHp.png')
-		.addFields(
-		{ name: 'GitHub System Status', value: body.status.description},
-		{ name: '\u200B', value: '\u200B' },
-		{ name: body.components[0].name, value: capitalizeFirstLetter(body.components[0].status), inline: true },
-		{ name: body.components[1].name, value: capitalizeFirstLetter(body.components[1].status), inline: true },
-		{ name: body.components[2].name, value: capitalizeFirstLetter(body.components[2].status), inline: true },
-		{ name: body.components[4].name, value: capitalizeFirstLetter(body.components[4].status), inline: true },
-		{ name: body.components[5].name, value: capitalizeFirstLetter(body.components[5].status), inline: true },
-		{ name: body.components[6].name, value: capitalizeFirstLetter(body.components[6].status), inline: true },
-		{ name: body.components[7].name, value: capitalizeFirstLetter(body.components[7].status), inline: true },
-		{ name: body.components[8].name, value: capitalizeFirstLetter(body.components[8].status), inline: true },
-		)
+		.setTitle('Howard Bot GitHub')
+		.setURL('https://github.com/pforbeck/Howard-Discord-Bot')
 		.setTimestamp()
 		.setFooter('Brought to you by Howard', 'https://i.imgur.com/wdDNd67.png');
 		msg.channel.send(githubStatus);
-		});	
 	},
 };
--- commands/?commands.js
+++ commands/?commands.js
@@ -9,9 +9,9 @@ module.exports = {
 		.setAuthor('Howard Bot', 'https://i.imgur.com/wdDNd67.png')
 		.setThumbnail('https://i.imgur.com/wdDNd67.png')
 		.addFields(
-		{ name: 'Commands', value: '?commands\nhoward\nape escape\nboth of\ncandice\ncd\ndragon\nxqc', inline: true},
-		{ name: '-', value: 'imagine dragons\nlick my\noh really\nparody\npong\nsadge\nstigma\nfizzle', inline: true},
-		{ name: '-', value: '\ngetka\nhandful\nharambe\nhorny\nsuck my\nwendys\nwho asked\n!github', inline: true},
+		{ name: 'Commands', value: '?commands\nhoward\nape escape\nboth of\ncandice\ndragon\nxqc\n!github', inline: true},
+		{ name: '-', value: 'imagine dragons\nlick my\noh really\nparody\npong\nsadge\nstigma\nfizzle\ncd', inline: true},
+		{ name: '-', value: '\ngetka\nhandful\nharambe\nhorny\nsuck my\nwendys\nwho asked\n!githubstatus', inline: true},
 		)
 		.setTimestamp()
 		.setFooter('Brought to you by Howard', 'https://i.imgur.com/wdDNd67.png');
